c94e2acbdeb57da556af02f348e161b4d00c5fb1
Roy Kolak <roy.kolak@gmail.com>
2023-08-16 14:26:07 -0500

    Demo layered macros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,58 @@ import BigdotsDisplay, {
 export default function Home() {
   return (
     <>
-      <BigdotsDisplay
-        layers={[textMacro({ text: "hi", alignment: "center" })]}
-        dimensions={{ height: 16, width: 64 }}
-      />
-      <BigdotsDisplay
-        layers={[solidColorMacro({ color: "#00FFFF" })]}
-        dimensions={{ height: 16, width: 64 }}
-      />
-      <BigdotsDisplay
-        layers={[twinkleMacro({ speed: 100 })]}
-        dimensions={{ height: 16, width: 64 }}
-      />
-      <BigdotsDisplay
-        layers={[meteorShowerMacro({ color: "#FF0000" })]}
-        dimensions={{ height: 16, width: 64 }}
-      />
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[
+            solidColorMacro({ color: "#FF0000" }),
+            textMacro({ text: "hi", alignment: "left" }),
+            textMacro({
+              text: "hi",
+              alignment: "right",
+            }),
+            textMacro({
+              text: "hi",
+              alignment: "center",
+            }),
+          ]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[solidColorMacro({ color: "#00FFFF" })]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[
+            twinkleMacro({ speed: 100, color: "#AAA" }),
+            textMacro({
+              text: "hello!!",
+              alignment: "right",
+              color: "#FFFF00",
+              startingRow: 1,
+            }),
+          ]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[
+            meteorShowerMacro({ color: "#FF0000" }),
+            textMacro({
+              text: "THIS IS A TEST",
+              font: "system-6",
+              alignment: "center",
+              color: "#FFFFFF",
+              startingRow: 1,
+            }),
+          ]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
     </>
   );
 }
