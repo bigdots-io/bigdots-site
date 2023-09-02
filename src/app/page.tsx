ad04b0b123cd4293c36b153300d185a611a584fa
Roy Kolak <roy.kolak@gmail.com>
2023-09-02 16:23:24 -0500

    Add marquee demo and brightness support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import BigdotsDisplay, {
-  meteorShowerMacro,
-  solidColorMacro,
-  textMacro,
-  twinkleMacro,
+  marquee,
+  meteorShower,
+  solidColor,
+  text,
+  twinkle,
 } from "@bigdots-io/react-client";
 import { useEffect, useState } from "react";
 
@@ -22,13 +23,14 @@ export default function Home() {
       <div style={{ margin: 10 }}>
         <BigdotsDisplay
           layers={[
-            solidColorMacro({ color: "#FF0000" }),
-            textMacro({ text: "hi", alignment: "left" }),
-            textMacro({
+            solidColor({ color: "#FF0000" }),
+            text({ text: "hi", alignment: "left", brightness: 4 }),
+            text({
               text: "hi",
               alignment: "right",
+              brightness: 7,
             }),
-            textMacro({
+            text({
               text: "hi",
               alignment: "center",
             }),
@@ -38,19 +40,41 @@ export default function Home() {
       </div>
       <div style={{ margin: 10 }}>
         <BigdotsDisplay
-          layers={[solidColorMacro({ color: "#00FFFF" })]}
-          dimensions={{ height: 16, width: 64 }}
-        />
-      </div>
-      <div style={{ margin: 10 }}>
-        <BigdotsDisplay
           layers={[
-            twinkleMacro({ speed: 100, color: "#AAA" }),
-            textMacro({
-              text: "hello!!",
-              alignment: "right",
+            solidColor({
+              color: "#228B22",
+              width: 5,
+              height: 5,
+              startingColumn: 5,
+              startingRow: 10,
+            }),
+            solidColor({
+              color: "#FF0000",
+              width: 5,
+              height: 5,
+              startingColumn: 20,
+              startingRow: 10,
+            }),
+            solidColor({
+              color: "#00FFFF",
+              width: 50,
+              height: 10,
+              startingColumn: 3,
+              startingRow: 2,
+            }),
+            solidColor({
+              color: "#FFC0CB",
+              width: 5,
+              height: 5,
+              startingColumn: 15,
+              startingRow: 10,
+            }),
+            solidColor({
               color: "#FFFF00",
-              startingRow: 1,
+              width: 50,
+              height: 3,
+              startingColumn: 10,
+              startingRow: 5,
             }),
           ]}
           dimensions={{ height: 16, width: 64 }}
@@ -59,8 +83,23 @@ export default function Home() {
       <div style={{ margin: 10 }}>
         <BigdotsDisplay
           layers={[
-            meteorShowerMacro({ color: "#FF0000" }),
-            textMacro({
+            twinkle({ speed: 100, color: "#AAA" }),
+            text({
+              text: "hello!!",
+              alignment: "right",
+              color: "#FFFF00",
+              startingRow: 1,
+              width: 62,
+            }),
+          ]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[
+            meteorShower({ color: "#FF0000" }),
+            text({
               text: "THIS IS A TEST",
               font: "system-6",
               alignment: "center",
@@ -74,14 +113,26 @@ export default function Home() {
       <div style={{ margin: 10 }}>
         <BigdotsDisplay
           layers={[
-            solidColorMacro({ color: trigger ? "#228B22" : "#FF0000" }),
-            textMacro({
+            solidColor({
+              color: trigger ? "#228B22" : "#FF0000",
+              brightness: 7,
+            }),
+            text({
               text: trigger ? "Pass" : "Fail",
               font: "system-16",
               alignment: "center",
               color: "#FFFFFF",
               startingRow: 1,
             }),
+          ]}
+          dimensions={{ height: 16, width: 64 }}
+        />
+      </div>
+      <div style={{ margin: 10 }}>
+        <BigdotsDisplay
+          layers={[
+            solidColor({ color: "#B58B00" }),
+            marquee({ color: "#FFF", speed: 100 }),
           ]}
           dimensions={{ height: 16, width: 64 }}
         />
